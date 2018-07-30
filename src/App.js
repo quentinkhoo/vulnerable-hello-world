ae73c7939dbeb331d034aace0232a2230846fdef
Quentin Khoo <quentinkhoo@outlook.com>
2018-07-30 15:53:09 +0800

    Next commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import electron from 'electron';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <h2>This is your first react app!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
